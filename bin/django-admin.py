7113cf795cd697e09b317e40865f1d0b819f2ed3
Fernanda Alves <fernanda__m@hotmail.com>
2020-08-24 11:44:07 -0300

    models

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/fernandaalves/Desktop/Development/DjangoProjects/url_shortener/urlvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
